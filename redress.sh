83a815dd5e7941fa71169d1fa01da66e3b9d8a97
Josef Fröhle <github@josef-froehle.de>
2017-05-17 19:44:32 +0200

    Move config/template.ini to config/$1.ini (#2)
    * Move template.ini to $1.ini

--- redress.sh
+++ redress.sh
@@ -17,6 +17,8 @@ sed -i '' \
     -e "s/_template/_${1}/g" \
     -e "s/template\.ini/$1.ini/" \
     index.js
+    
+mv config/template.ini config/$1.ini
 
 git add package.json README.md index.js test/index.js 
 git commit -m "renamed template to $1"
